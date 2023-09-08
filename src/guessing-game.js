bb1894079c5062103c197fb5244c0c61ccd446cd
totoogg <goncharov.vova2014@gamil.com>
2023-09-08 17:03:55 +0300

    feat: problem guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.min +  Math.ceil((this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.max - Math.floor((this.max - this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.min + Math.ceil((this.max - this.min) / 2);
     }
 }
 
